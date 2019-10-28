adc2141e6b478efd069f3b52f5a5759be2687650
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2019-10-28 13:34:29 -0300

    MatSnackBarModule added in app.moule.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { AppComponent } from './app.component';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { GameService } from './game.service';
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     MatSliderModule,
+    MatSnackBarModule,
     MatGridListModule,
     TooltipModule.forRoot(),
     BrowserAnimationsModule
